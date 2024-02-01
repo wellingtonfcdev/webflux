e06560f9d725c2f366865512ccb85c130aadc1e5
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-02-01 11:18:35 -0300

    Start do projeto WebFlux

---
+++ src/main/java/br/com/wellingtondev/webfluxcourse/WebFluxCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.wellingtondev.webfluxcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxCourseApplication.class, args);
+    }
+
+}
